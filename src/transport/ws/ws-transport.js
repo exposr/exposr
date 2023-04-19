6b629d05aaa31cbdcf47a6571f7b9e08a1faf8f5
Fredrik Lindberg <fli@fli.dev>
2023-04-19 19:18:48 +0200

    build(deps): upgrade ws to 8.13.0

--- src/transport/ws/ws-transport.js
+++ src/transport/ws/ws-transport.js
@@ -2,7 +2,7 @@ import assert from 'assert/strict';
 import { ECONNREFUSED, EINPROGRESS, EMFILE, EPIPE, ETIMEDOUT } from 'constants';
 import { EventEmitter } from 'events';
 import { Duplex } from 'stream';
-import WebSocket from 'ws';
+import WebSocket, { createWebSocketStream } from 'ws';
 import Console from '../../console/index.js';
 import CustomError from '../../utils/errors.js';
 
@@ -34,7 +34,7 @@ class WebSocketTransport extends EventEmitter {
         this._socket = opts.socket;
         assert(this._socket !== undefined);
         this._tunnelId = opts.tunnelId;
-        this._socketStream = WebSocket.createWebSocketStream(this._socket, {
+        this._socketStream = createWebSocketStream(this._socket, {
             objectMode: false,
             readableObjectMode: false,
             writableObjectMode: false,
